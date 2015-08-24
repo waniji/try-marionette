25125387b4d1605ec184dc37175394b02307d7f8
Taga Yasunori <yasukun.taga@gmail.com>
2015-08-24 18:02:47 +0900

    ...

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,46 @@
-var App = new Marionette.Application();
+$(function(){
 
-App.onStart = function() {
-    console.log("Let's start!");
-    console.log("HiTECH!");
-};
+    // 大元のappインスタンスを作成
+    var Manager = new Marionette.Application();
 
-App.start();
+    var Region = Marionette.Region.extend({
+      attachHtml: function(view){
+        console.log("Atach Sareta!!!");
+        this.$el.empty().append(view.el);
+       }
+     })
+
+    Manager.addRegions({
+        mainRegion: {
+          regionClass: Region,
+          selector: '#main-region'
+        }
+    });
+
+    Manager.UserView = Marionette.ItemView.extend({
+        template: '#user-form-template',
+    });
+
+    Manager.User = Backbone.Model.extend({
+    });
+
+    Manager.onStart = function(){
+
+        // viewにセットするmodelのインスタンス
+        var alice = new Manager.User({
+                        firstName: 'Alice',
+                        });
+
+        // viewのインスタンス
+        var aliceView = new Manager.UserView({
+            model: alice
+        });
+
+        Manager.mainRegion.show(aliceView);
+
+    };
+
+    Manager.start();
+
+});
 
