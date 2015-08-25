41878c77d7d16ddb33b6b883561976d9397763db
waniji <wanigoya@gmail.com>
2015-08-25 16:53:51 +0900

    Add dummy data

---
+++ src/js/data.js
@@ -0,0 +1,7 @@
+module.exports = {
+    users: [
+        { id: 1, firstName: "Spencer", lastName: "Offley" },
+        { id: 2, firstName: "Tobias", lastName: "Bott" },
+        { id: 3, firstName: "Robin", lastName: "Harington" }
+    ]
+}
