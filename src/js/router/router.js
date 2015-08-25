4ed30e3136ed7e580070ba34a92bb8d391cec961
waniji <wanigoya@gmail.com>
2015-08-25 16:53:36 +0900

    Add AppRouter

---
+++ src/js/router/router.js
@@ -0,0 +1,38 @@
+var User = require('../model/user');
+var Users = require('../collection/users');
+var ShowUserView = require('../view/show_user');
+var ListUsersView = require('../view/list_users');
+var UserView = require('../view/user');
+var Data = require('../data');
+
+module.exports = Marionette.AppRouter.extend({
+    routes: {
+        "": "users",
+        "users": "users",
+        "users/new": "createUser",
+        "users/:id": "showUser",
+        "users/:id/edit": "editUser"
+    },
+
+    users: function() {
+        var users = new Users(Data.users);
+
+        var listUsersView = new ListUsersView({
+            collection: users,
+            childView: UserView
+        });
+
+        Manager.mainRegion.show(listUsersView);
+    },
+
+    showUser: function(id) {
+        user = new User(Data.users[id-1]);
+        userView = new ShowUserView({ model: user });
+
+        Manager.mainRegion.show(userView);
+    },
+
+    editUser: function(id) {
+    }
+});
+
